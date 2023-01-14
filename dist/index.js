239772944299cd57be55241d439bbda40584a7ca
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-01-14 22:56:07 +0200

    start of logic

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("hello world");
+function main() {
+    var _a;
+    const buttons = (_a = document.querySelector(".buttons-container")) === null || _a === void 0 ? void 0 : _a.childNodes;
+    const field = document.querySelector(".input");
+    buttons === null || buttons === void 0 ? void 0 : buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if (field != null) {
+                field.textContent = btn.textContent;
+            }
+        });
+    });
+}
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+function main(){
+    const buttons = document.querySelector(".buttons-container")?.childNodes;
+    const field = document.querySelector(".input");
+    buttons?.forEach((btn) => {
+        btn.addEventListener("click",  () => {
+            if(field != null){
+                field.textContent = btn.textContent;
+            }
+        })
+    })
+}
+
+main();
