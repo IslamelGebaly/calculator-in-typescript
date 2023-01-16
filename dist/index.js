5e9516912c2a44eb3574c00de3e050361f05ed69
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-01-16 22:59:50 +0200

    number and floating point handeling

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,32 @@
 "use strict";
 function main() {
-    var _a;
-    const buttons = (_a = document.querySelector(".buttons-container")) === null || _a === void 0 ? void 0 : _a.childNodes;
+    const numberBtns = document.querySelectorAll(".number-btn");
+    const pointBtn = document.querySelector("#pnt");
     const field = document.querySelector(".input");
-    buttons === null || buttons === void 0 ? void 0 : buttons.forEach((btn) => {
+    let isOverwritable = true;
+    let isFloat = false;
+    let fieldText;
+    numberBtns === null || numberBtns === void 0 ? void 0 : numberBtns.forEach((btn) => {
         btn.addEventListener("click", () => {
-            if (field != null) {
-                field.textContent = btn.textContent;
+            if ((field === null || field === void 0 ? void 0 : field.textContent) != null) {
+                if (isOverwritable) {
+                    field.textContent = "";
+                    isOverwritable = false;
+                }
+                fieldText = field.textContent + btn.textContent;
+                field.textContent = fieldText;
             }
         });
+    });
+    pointBtn === null || pointBtn === void 0 ? void 0 : pointBtn.addEventListener("click", () => {
+        if (!isFloat) {
+            isFloat = true;
+            if ((field === null || field === void 0 ? void 0 : field.textContent) != null) {
+                isOverwritable = false;
+                fieldText = field.textContent + pointBtn.textContent;
+                field.textContent = fieldText;
+            }
+        }
     });
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 function main(){
-    const buttons = document.querySelector(".buttons-container")?.childNodes;
+    const numberBtns = document.querySelectorAll(".number-btn");
+    const pointBtn = document.querySelector("#pnt");
     const field = document.querySelector(".input");
-    buttons?.forEach((btn) => {
+
+    let isOverwritable : boolean = true;
+    let isFloat : boolean = false;
+    let fieldText : string;
+
+    numberBtns?.forEach((btn) => {
         btn.addEventListener("click",  () => {
-            if(field != null){
-                field.textContent = btn.textContent;
+            if(field?.textContent != null){
+                if(isOverwritable){
+                    field.textContent = "";
+                    isOverwritable = false;
+                }
+
+                fieldText = field.textContent + btn.textContent;
+                field.textContent = fieldText;
             }
         })
-    })
+    });
+
+    pointBtn?.addEventListener("click", () => {
+        if(!isFloat){
+            isFloat = true;
+            if(field?.textContent != null){
+                isOverwritable = false;
+                fieldText = field.textContent + pointBtn.textContent;
+                field.textContent = fieldText;
+            }
+        }
+    });
 }
 
 main();
