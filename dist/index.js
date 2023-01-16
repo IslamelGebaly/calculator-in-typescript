eeaa49f4b5aab9d84d0fc2ab81b888b64fd0cb10
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-01-16 23:28:28 +0200

    basic operations

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,17 @@
 function main() {
     const numberBtns = document.querySelectorAll(".number-btn");
     const pointBtn = document.querySelector("#pnt");
+    const addBtn = document.querySelector("#plus");
+    const subBtn = document.querySelector("#minus");
+    const multBtn = document.querySelector("#mult");
+    const divBtn = document.querySelector("#div");
     const field = document.querySelector(".input");
+    const outputField = document.querySelector(".output");
     let isOverwritable = true;
     let isFloat = false;
+    let isOutputSet = false;
     let fieldText;
+    let finalValue = 0;
     numberBtns === null || numberBtns === void 0 ? void 0 : numberBtns.forEach((btn) => {
         btn.addEventListener("click", () => {
             if ((field === null || field === void 0 ? void 0 : field.textContent) != null) {
@@ -27,6 +34,50 @@ function main() {
                 field.textContent = fieldText;
             }
         }
+    });
+    addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", () => {
+        isOverwritable = true;
+        if (!isOutputSet) {
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue += Number.parseFloat(fieldText);
+        if (outputField != null)
+            outputField.textContent = finalValue.toString();
+    });
+    subBtn === null || subBtn === void 0 ? void 0 : subBtn.addEventListener("click", () => {
+        isOverwritable = true;
+        if (!isOutputSet) {
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue -= Number.parseFloat(fieldText);
+        if (outputField != null)
+            outputField.textContent = finalValue.toString();
+    });
+    multBtn === null || multBtn === void 0 ? void 0 : multBtn.addEventListener("click", () => {
+        isOverwritable = true;
+        if (!isOutputSet) {
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue *= Number.parseFloat(fieldText);
+        if (outputField != null)
+            outputField.textContent = finalValue.toString();
+    });
+    divBtn === null || divBtn === void 0 ? void 0 : divBtn.addEventListener("click", () => {
+        isOverwritable = true;
+        if (!isOutputSet) {
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue /= Number.parseFloat(fieldText);
+        if (outputField != null)
+            outputField.textContent = finalValue.toString();
     });
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 function main(){
     const numberBtns = document.querySelectorAll(".number-btn");
     const pointBtn = document.querySelector("#pnt");
+
+    const addBtn = document.querySelector("#plus");
+    const subBtn = document.querySelector("#minus");
+    const multBtn = document.querySelector("#mult");
+    const divBtn = document.querySelector("#div");
+
     const field = document.querySelector(".input");
+    const outputField = document.querySelector(".output");
 
     let isOverwritable : boolean = true;
     let isFloat : boolean = false;
+    let isOutputSet : boolean = false;
+
     let fieldText : string;
+    let finalValue : number = 0;
+
 
     numberBtns?.forEach((btn) => {
         btn.addEventListener("click",  () => {
@@ -30,6 +41,61 @@ function main(){
                 field.textContent = fieldText;
             }
         }
+    });
+
+    addBtn?.addEventListener("click", () => {
+        isOverwritable = true;
+        if(!isOutputSet){
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue += Number.parseFloat(fieldText);
+        if(outputField != null)
+            outputField.textContent = finalValue.toString();
+        
+    });
+
+    subBtn?.addEventListener("click", () => {
+        isOverwritable = true;
+
+        if(!isOutputSet){
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue -= Number.parseFloat(fieldText);
+
+        if(outputField != null)
+            outputField.textContent = finalValue.toString();
+    });
+
+    multBtn?.addEventListener("click", () => {
+        isOverwritable = true;
+
+        if(!isOutputSet){
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue *= Number.parseFloat(fieldText);
+
+        if(outputField != null)
+            outputField.textContent = finalValue.toString();
+    });
+
+    divBtn?.addEventListener("click", () => {
+        isOverwritable = true;
+
+        if(!isOutputSet){
+            finalValue = Number.parseFloat(fieldText);
+            isOutputSet = true;
+        }
+        else
+            finalValue /= Number.parseFloat(fieldText);
+
+        if(outputField != null)
+            outputField.textContent = finalValue.toString();
     });
 }
 
