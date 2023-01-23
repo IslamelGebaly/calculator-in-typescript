f5630cd7b9d53924611553a9f347f76271164165
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-01-23 23:04:15 +0200

    operation ui

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,13 @@
 import { Calculator } from "./calculator.js";
 let isOverwritable = true;
-let isFloat = false;
+let isDecimal = false;
 let isOutputSet = false;
 function main() {
     const calc = new Calculator();
     const inputField = document.querySelector(".input");
     const opField = document.querySelector(".output");
     const numberBtns = document.querySelectorAll(".number-btn");
+    const pointBtn = document.querySelector("#pnt");
     numberBtns?.forEach((btn) => {
         btn.addEventListener("click", () => {
             if (inputField?.textContent != null)
@@ -18,5 +19,57 @@ function main() {
                     inputField.textContent += btn.textContent;
         });
     });
+    pointBtn?.addEventListener("click", () => {
+        if (inputField?.textContent != null) {
+            if (!isDecimal) {
+                inputField.textContent += ".";
+                isDecimal = true;
+                isOverwritable = false;
+            }
+        }
+    });
+    initOperationButtons(inputField, opField);
+}
+function initOperationButtons(inputField, opField) {
+    const plusBtn = document.querySelector('#plus');
+    const minusBtn = document.querySelector('#minus');
+    const multBtn = document.querySelector('#mult');
+    const divBtn = document.querySelector('#div');
+    plusBtn?.addEventListener("click", () => {
+        if (opField?.textContent != null) {
+            if (!isOverwritable) {
+                resetInput();
+                opField.textContent += (inputField?.textContent + "+");
+            }
+        }
+    });
+    minusBtn?.addEventListener("click", () => {
+        if (opField?.textContent != null) {
+            if (!isOverwritable) {
+                resetInput();
+                opField.textContent += (inputField?.textContent + "-");
+            }
+        }
+    });
+    multBtn?.addEventListener("click", () => {
+        if (opField?.textContent != null) {
+            if (!isOverwritable) {
+                resetInput();
+                opField.textContent += (inputField?.textContent + "x");
+            }
+        }
+    });
+    divBtn?.addEventListener("click", () => {
+        if (opField?.textContent != null) {
+            if (!isOverwritable) {
+                resetInput();
+                opField.textContent += (inputField?.textContent + "÷");
+            }
+        }
+    });
+}
+function resetInput() {
+    isDecimal = false;
+    isOverwritable = true;
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Calculator } from "./calculator.js";
 
 let isOverwritable : boolean = true;
-let isFloat : boolean = false;
+let isDecimal : boolean = false;
 let isOutputSet : boolean = false; 
 
 function main(){
@@ -11,6 +11,7 @@ function main(){
     const opField : Element | null = document.querySelector(".output");
 
     const numberBtns : NodeListOf<HTMLElement> | null = document.querySelectorAll(".number-btn");
+    const pointBtn : Element | null = document.querySelector("#pnt");
 
     numberBtns?.forEach((btn) => {
         btn.addEventListener("click" , () =>{
@@ -23,6 +24,67 @@ function main(){
                 inputField.textContent += btn.textContent;
         });
     });
+
+    pointBtn?.addEventListener("click", () => {
+        if(inputField?.textContent != null){
+            if(!isDecimal){
+                inputField.textContent += ".";
+                isDecimal = true;
+                isOverwritable = false;
+            }
+        }
+    });
+
+    initOperationButtons(inputField, opField);
+    
+}
+
+function initOperationButtons(inputField : Element | null, opField : Element | null){
+    const plusBtn : Element | null = document.querySelector('#plus');
+    const minusBtn : Element | null = document.querySelector('#minus');
+    const multBtn : Element | null = document.querySelector('#mult');
+    const divBtn : Element | null = document.querySelector('#div');
+
+    plusBtn?.addEventListener("click", () => {
+        if(opField?.textContent != null){
+            if(!isOverwritable){
+                resetInput();
+                opField.textContent += (inputField?.textContent + "+");
+            } 
+        }
+    });
+
+    minusBtn?.addEventListener("click", () => {
+        if(opField?.textContent != null){
+            if(!isOverwritable){
+                resetInput();
+                opField.textContent += (inputField?.textContent + "-");
+            } 
+        }
+    });
+
+    multBtn?.addEventListener("click", () => {
+        if(opField?.textContent != null){
+            if(!isOverwritable){
+                resetInput();
+                opField.textContent += (inputField?.textContent + "x");
+            } 
+        }
+    });
+
+    divBtn?.addEventListener("click", () => {
+        if(opField?.textContent != null){
+            if(!isOverwritable){
+                resetInput();
+                opField.textContent += (inputField?.textContent + "÷");
+            } 
+        }
+    });
+}
+
+function resetInput(){
+    isDecimal = false;
+    isOverwritable = true;
 }
 
 main();
