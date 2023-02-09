35f0f231ebbcbbaaff0b6be050cdc92c14db37d7
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-02-09 21:16:16 +0200

    added parser.ts file

---
+++ src/parser.ts
@@ -0,0 +1,17 @@
+class Parser {
+    private opStack : String[];
+    private stack : String[];
+
+    constructor(){
+        this.opStack = [];
+        this.stack = [];
+    }
+
+    parse(input : String) : void{
+        
+    }
+
+    pop() : String {
+        return "";
+    } 
+}
